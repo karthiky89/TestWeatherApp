46bb9dbc7468cf0322c352a732560a6c49bd300a
karthikYalamanchili <c_kyalamanchili@costco.com>
2024-08-19 18:31:36 -0500

    Initial Commit

---
+++ TestWeatherApp/TestWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherAppApp.swift
+//  TestWeatherApp
+//
+//  Created by Karthik Yalamanchili on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
